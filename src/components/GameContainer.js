a5d8bcffc3430a19f4d66476b9ea5d1dac30867a
Mateo <madiazt@unal.edu.co>
2021-06-11 20:49:19 -0500

    added skeleton of console

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,12 +1,18 @@
 import Console from "./gameComponents/Console"
 import CardsContainer from "./gameComponents/CardsContainer"
+import React from "react"
 
 const GameContainer = () => {
+    const [historicRecord, set_historicRecord] = React.useState(0);
+    const [currentCount, set_currentCount] = React.useState(0);
 
     return (
         <div className="row">
             {/*include console and the cards container*/}
-            <Console />
+            <Console 
+            historic={historicRecord}
+            cCount={currentCount}
+            />
             <CardsContainer />
         </div>
     )
--- src/components/gameComponents/Console.js
+++ src/components/gameComponents/Console.js
@@ -1,10 +1,14 @@
 const Console = (props) => {
+
     return(
         <div className="row">
             <span>Console:</span>
             {/*Historic record*/}
+            <span>Historic Record: {props.historic}</span>
 
             {/*Current count*/}
+            <span>Current Count: {props.cCount}</span>
+
         </div>
     )
 }
